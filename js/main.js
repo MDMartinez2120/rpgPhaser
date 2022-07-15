bb36b938f216d340e4ab4222934e9abaef44fe3f
Matthew Martinez <matt.martinez2120@gmail.com>
2022-07-15 15:47:45 -0500

    feat: fixed players walk animation.

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ let config = {
         default: "arcade",
         arcade: {
             gravity: {y: 300},
-           debug: false
+            debug: false
         }
     },
     scene: {
@@ -27,46 +27,56 @@ let keyW;
 
 function preload(){
     this.load.image('background', '/assets/Backdrop.png');
-    this.load.spritesheet('player', '/assets/sprites/Hero.png', {frameWidth: 32, frameHeight: 32});
+    this.load.spritesheet('player', '/assets/sprites/Herofull.png', {frameWidth: 32, frameHeight: 32});
     this.load.image('platform', '/assets/Platform.png');
 
 }
 
 function create(){
-    //BACKGROUND
-    let backdrop = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'background');
-    let scaleX = this.cameras.main.width / backdrop.width
-    let scaleY = this.cameras.main.height / backdrop.height
-    let scale = Math.max(scaleX, scaleY);
-    backdrop.setScale(scale).setScrollFactor(0);
+    //BACKGROUND\\
+    this.add.image(400, 300, 'background').setScale(5);
 
-    //PLATFORMS
+    //PLATFORMS\\
     platforms = this.physics.add.staticGroup();
 
-    platforms.create(600, 400, 'platform').setScale(5);
-    platforms.create(150, 350, 'platform').setScale(5);
+    platforms.create(400, 530,  'platform');
+
+    platforms.create(600, 400, 'platform');
+    platforms.create(50, 250, 'platform');
+    platforms.create(750, 220, 'platform');
 
 
 
-    //CHARACTER SPRITE
+    //CHARACTER SPRITE\\
     this.anims.create({
-        key: 'walk',
-        frames: this.anims.generateFrameNumbers('player', { start: 0, end: 1 }),
-        frameRate: 2,
-        repeat: -1
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('player', { start: 4, end: 6 }),
+        frameRate: 3,
+        repeat: 0
     });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [{key: 'player', frame: 3}],
+        frameRate:1
+    });
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('player', { start: 0, end: 2 }),
+        frameRate: 3,
+        repeat: 0
+    });
+
+    //CHARACTER PHYSICS\\
     hero = this.physics.add.sprite(100, 450, 'player');
-    hero.play('walk');
     hero.body.setGravityY(300);
     hero.setScale(3);
     hero.setBounce(0.2);
     hero.setCollideWorldBounds(true);
-
-
-
     this.physics.add.collider(hero, platforms)
 
-    //MOVEMENT CONTROLS
+    //MOVEMENT CONTROLS\\
     keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
     keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
@@ -78,12 +88,13 @@ function update(){
 
     if(keyA.isDown) {
         hero.x = hero.x - 2;
+        hero.anims.play('left', true);
     } else if(keyS.isDown) {
         hero.y = hero.y + 2;
     } else if(keyD.isDown) {
         hero.x = hero.x + 2;
-    } else if(keyW.isDown || keyD.isDown) {
+        hero.anims.play('right', true);
+    } else if(keyW.isDown) {
         hero.y = hero.y - 17;
     }
-
 }
